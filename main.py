22f603811eac1df3733351efaac7e51c0c7eebda
Dmitry Morozov <9984398@gmail.com>
2022-02-06 15:01:41 +0300

    Class Coord, Class Circle : Implemented

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+class Coord:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Circle:
+    def __init__(self, coord, radius):
+        self.radius = radius
+        self.coord = coord
+
 
 def get_two_circles():
     """ Returns two circle """
 
     pass
-
