1cc85d52ccf50370f2e137b9dcaa19986122cf70
Dmitry Morozov <9984398@gmail.com>
2022-02-10 20:26:06 +0300

    feat(main.py): Cover_by_circles() : Need to fix.

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ class Point:
         self.x = x
         self.y = y
 
+    def __str__(self):
+        return "({}, {})".format(self.x, self.y)
+
 
 class Circle:
+
     def __init__(self, coord, radius):
         self.radius = radius
         self.center = coord
@@ -26,6 +30,38 @@ def get_distance(p1, p2):
     Return the distance between the two points
     """
     return ((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2) ** 0.5
+
+
+def cover_by_circles(coord_array):
+    """
+    coord_array : array of Coord
+    Return the two circles that cover the points
+    """
+    center_point = find_center_point(coord_array)
+    print(f"Center point: {center_point}")
+
+    left, right = split_set_by_point(coord_array, center_point)
+    print(f"Left: {left}")
+    print(f"Right: {right}")
+
+    v_left_center_point = find_center_point(left)  # Find Virtual Center Point of the left set
+    print(f"Virtual Center Point of the left set: {v_left_center_point}")
+    v_right_center_point = find_center_point(right)  # Find Virtual Center Point of the right set
+    print(f"Virtual Center Point of the right set: {v_right_center_point}")
+
+    left_center_point = find_nearest_point(left, v_left_center_point)  # Find the nearest point to the virtual center
+    # point of the left set
+    print(f"Left Center Point: {left_center_point}")
+    right_center_point = find_nearest_point(right, v_right_center_point)  # Find the nearest point to the virtual
+    # center point of the right set
+    print(f"Right Center Point: {right_center_point}")
+
+    left_radius = get_distance(find_far_point(left, left_center_point),
+                               left_center_point)  # Find the radius of the left circle
+    right_radius = get_distance(find_far_point(right, right_center_point),
+                                right_center_point)  # Find the radius of the right circle
+
+    return Circle(left_center_point, left_radius), Circle(right_center_point, right_radius)
 
 
 def draw_result(coord_array, circle1, circle2):
@@ -72,12 +108,12 @@ def draw_result(coord_array, circle1, circle2):
     plt.show()
 
 
-def get_sorted_points(coord_array):
-    """
-    coord_array : array of Coord
-    Return the array sorted by the distance between the point and the center of the circle
-    """
-    return sorted(coord_array, key=lambda coord: (coord.x, coord.y))
+# def get_sorted_points(coord_array):
+#     """
+#     coord_array : array of Coord
+#     Return the array sorted by the distance between the point and the center of the circle
+#     """
+#     return sorted(coord_array, key=lambda coord: (coord.x, coord.y))
 
 
 def find_center_point(coord_array):
@@ -109,6 +145,24 @@ def split_set_by_point(coord_array, point):
     return left, right
 
 
+def find_nearest_point(coord_array, point):
+    """
+    coord_array : array of Coord
+    point : Coord
+    Return the nearest point to the point
+    """
+    return min(coord_array, key=lambda coord: get_distance(coord, point))
+
+
+def find_far_point(coord_array, point):
+    """
+    coord_array : array of Coord
+    point : Coord
+    Return the farthest point to the point
+    """
+    return max(coord_array, key=lambda coord: get_distance(coord, point))
+
+
 if __name__ == "__main__":
     points_array = [
         Point(-4, -7),
@@ -118,22 +172,18 @@ if __name__ == "__main__":
         Point(3, -5),
         Point(4, 4),
         ]
+    points_array_1 = [
+        Point(0, 0),
+        Point(1, 1),
+        Point(-1, -1),
+        Point(-2, -2),
+        Point(5, 5),
+        Point(6, 6),
+        Point(7, 7),
+        Point(8, 8),
+        ]
 
-    c1 = Circle(Point(1, 1), 3)
-    c2 = Circle(Point(3, 3), 2)
+    c1, c2 = cover_by_circles(points_array_1)
+print(c1, c2)
 
-    print(c1, c2, sep="\n")
-    sorted_points = get_sorted_points(points_array)
-
-    for point in sorted_points:
-        print(f"point :  ({point.x}, {point.y})")
-
-    # draw_result(points_array, c1, c2)
-    draw_result(sorted_points, c1, c2)
-
-    left, right = split_points_set(sorted_points)
-    for point in left:
-        print(f"Left point :  ({point.x}, {point.y})")
-
-    for point in right:
-        print(f"Right point :  ({point.x}, {point.y})")
+draw_result(points_array_1, c1, c2)
