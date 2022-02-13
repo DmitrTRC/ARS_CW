92e6411a1dce85840ff71944ee06d94b899537a9
Dmitry Morozov <9984398@gmail.com>
2022-02-13 12:34:09 +0300

    Prepared for lesson.

--- Tests/test_helper.py
+++ Tests/test_helper.py
@@ -4,6 +4,5 @@ from logic import helper as h
 class TesHelper:
 
     def test_get_distance(self):
-        assert h.get_distance(0, 0, 0, 0) == 0
-        assert h.get_distance(1,2,3,4) == 5
+        pass
         
