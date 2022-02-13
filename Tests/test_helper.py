a9c91f025664704c9643e5137af1108f394fd1cf
Dmitry Morozov <9984398@gmail.com>
2022-02-13 14:16:08 +0300

    Refactored

--- Tests/test_helper.py
+++
@@ -1,9 +0,0 @@
-from logic import helper as h
-
-
-class TesHelper:
-
-    def test_get_distance(self):
-        assert h.get_distance(0, 0, 0, 0) == 0
-        assert h.get_distance(1,2,3,4) == 5
-        
