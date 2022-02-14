57cb51300ee7663563d81b67c44d7b86f0e72036
Dmitry Morozov <9984398@gmail.com>
2022-02-14 14:09:58 +0300

    Test Passed.

--- Tests/test_helper.py
+++ Tests/test_helper.py
@@ -1,5 +1,15 @@
 from helper.helper import *
+from math import sqrt
 
 
 def test_get_distance():
-    assert False
+    assert get_distance(Point(0, 0), Point(0, 0)) == 0
+    assert get_distance(Point(0, 0), Point(1, 1)) == sqrt(2)
+    assert get_distance(Point(0, 0), Point(1, 0)) == 1
+    assert get_distance(Point(0, 0), Point(0, 1)) == 1
+    assert get_distance(Point(0, 0), Point(-1, -1)) == sqrt(2)
+    assert get_distance(Point(0, 0), Point(-1, 0)) == 1
+    assert get_distance(Point(0, 0), Point(0, -1)) == 1
+    assert get_distance(Point(1, 1), Point(2, 2)) == sqrt(2)
+    assert get_distance(Point(1, 1), Point(3, 3)) == sqrt(8)
+    assert get_distance(Point(1, 1), Point(4, 4)) == sqrt(18)
